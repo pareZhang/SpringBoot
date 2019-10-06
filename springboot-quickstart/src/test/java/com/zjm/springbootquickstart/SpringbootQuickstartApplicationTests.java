0472644a05427c0777789a62c9f0953891fa48ed
zjm <pareZhang@126.com>
2019-10-06 17:16:15 +0800

    YML配置以及将配置注入到bean中

--- springboot-quickstart/src/test/java/com/zjm/springbootquickstart/SpringbootQuickstartApplicationTests.java
+++ springboot-quickstart/src/test/java/com/zjm/springbootquickstart/SpringbootQuickstartApplicationTests.java
@@ -1,16 +1,25 @@
 package com.zjm.springbootquickstart;
 
+import com.zjm.springbootquickstart.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * 单元测试
+ * 可以在测试期间很方便的类似编码一样进行自动注入
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootQuickstartApplicationTests {
+    @Autowired
+    private Person person;
 
     @Test
     public void contextLoads() {
+        System.out.println(person);
     }
 
 }
