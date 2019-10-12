9136c2f4fcba386f344e9842c84e63a48cc18127
zjm <pareZhang@126.com>
2019-10-12 20:00:56 +0800

    redis单机缓存

--- redis/src/main/java/com/zjm/RedisApplication.java
+++ redis/src/main/java/com/zjm/RedisApplication.java
@@ -2,8 +2,10 @@ package com.zjm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RedisApplication {
 
 	public static void main(String[] args) {
--- redis/src/test/java/com/zjm/RedisApplicationTests.java
+++ redis/src/test/java/com/zjm/RedisApplicationTests.java
@@ -1,16 +1,33 @@
 package com.zjm;
 
+import com.zjm.bean.Book;
+import com.zjm.repository.BookDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RedisApplicationTests {
+    @Autowired
+    private BookDao bookDao;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        bookDao.getBookById(1);
+        bookDao.getBookById(1);
+        bookDao.deleteBookById(1);
+        Book b1 = bookDao.getBookById(1);
+        System.out.println("b1:" + b1);
+        Book b = new Book();
+        b.setName("三国演义");
+        b.setAuthor("罗贯中");
+        b.setId(1);
+        bookDao.updateBookById(b);
+        Book b2 = bookDao.getBookById(1);
+        System.out.println("b2:" + b2);
+    }
 
 }
